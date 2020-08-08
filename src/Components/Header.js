65a8f963ad5e2015aaba576061652d88e6c34ca9
Manas Kumar <65833852+manas1316am@users.noreply.github.com>
2020-08-08 11:46:27 -0400

    Add files via upload

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <header>
+            <h1 
+                style = {{
+                    fontWeight: "bold",
+                    fontSize: "40px",
+                    textAlign: "center"
+                }}
+            >
+                Theme Toggler
+            </h1>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/colors.js
@@ -0,0 +1,19 @@
+// For  defining the  light and dark color
+
+const ColorInit = {
+    // Light Theme
+    light: {
+        textColor: "#000",
+        backgroundColor: "#fff",
+        border: "2px solid #000"
+    },
+    
+    // Dark Theme
+    dark: {
+        textColor: "#fff",
+        backgroundColor: "#333", 
+        border: "2px dashed #000"
+    },
+};
+
+export default ColorInit;
